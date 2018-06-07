8deec7d1224e7326ed4c0c85fc8dd340a152df3f
liwenbo <815050565@163.com>
2018-06-07 14:48:12 +0800

    聊天测试

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
 	protected $table = 'group';
-	protected $primaryKey = 'group_id';
+	protected $primaryKey = 'id';
 	public $timestamps = false;
 	// 不可被批量赋值的属性，反之其他的字段都可被批量赋值
 	protected $guarded = [
-		'group_id'
+		'id'
 	];
 }
