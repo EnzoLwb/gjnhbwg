74c130c32b94f95fa2a3ffb42ee1ad2e18fd3b33
liwenbo <815050565@163.com>
2018-06-08 10:43:40 +0800

    不限制登录

--- routes/api_chat.php
+++ routes/api_chat.php
@@ -1,7 +1,7 @@
 <?php
 // 需要登录验证的路由
 Route::group([
-	'middleware' => 'auth:api'
+//	'middleware' => 'auth:api'
 ], function () {
 	//加入群组
 	Route::get('chat/create_group', 'GatewayController@create_group');
